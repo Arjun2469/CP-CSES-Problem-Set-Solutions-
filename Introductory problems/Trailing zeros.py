5c3051a3f3f77ab53e00614c6129f6f2c9e73e1f
Arjun <kesharwaniarjun246@gmail.com>
2022-03-22 12:10:27 +0530

    initial commit

---
+++ Introductory problems/Trailing zeros.py
@@ -0,0 +1,14 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+n=int(input())
+c=0
+while(n!=0):
+    n=n//5
+    c+=n
+print(c)
