550a2eb2bcf21c4f12174a494381264b92b2d3f2
Arjun <kesharwaniarjun246@gmail.com>
2022-03-24 00:26:29 +0530

    initial commit

---
+++ Introductory problems/Two knights.py
@@ -0,0 +1,15 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+n=int(input())
+for i in range(1,n+1):
+    total=((i*i)*(i*i-1))//2
+    notsafe=4*(i-1)*(i-2)
+    ans=total-notsafe
+    print(ans)
+        
